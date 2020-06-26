fabf9ea88ad0e41e7c4e45955a6d0135d823d371
Jonas Askeland <jonas.askeland@payex.com>
2020-06-26 15:05:10 +0200

    Add tail to boids shape

--- src/Boids.Simulation/Components/DrawableBoidComponent.cs
+++ src/Boids.Simulation/Components/DrawableBoidComponent.cs
@@ -12,11 +12,13 @@ namespace Boids.Simulation.Components
         {
             var random = new Random((int)DateTime.UtcNow.Ticks);
             var colour = new Color((byte) random.Next(15, 255), (byte) random.Next(15, 255), (byte) random.Next(50, 255));
-            _shape = new VertexArray(PrimitiveType.LineStrip, 4)
+            _shape = new VertexArray(PrimitiveType.LineStrip, 5)
             {
                 [0] = new Vertex(new Vector2f(-9, -6), colour),
                 [1] = new Vertex(new Vector2f(0, 0), colour),
-                [2] = new Vertex(new Vector2f(-9, 6), colour)
+                [2] = new Vertex(new Vector2f(-9, 6), colour),
+                [3] = new Vertex(new Vector2f(0, 0), colour),
+                [4] = new Vertex(new Vector2f(-15, 0), colour),
             };
         }
 
